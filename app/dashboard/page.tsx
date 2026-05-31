efee2589a01817033c69bc0f0693c75248a10181
Seu Nome <seu@email.com>
2026-05-30 21:25:09 -0300

    teste data atendimento

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -99,7 +99,7 @@ export default function Dashboard() {
           prioridade: paciente.prioridade,
           sala: paciente.sala,
           senha: paciente.senha,
-          data_atendimento: new Date().toISOString(),
+          data_atendimento: "2025-05-30 12:00:00",
         },
       ]);
 
